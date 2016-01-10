4efd2af18feca77c2fc9e00c10e27185a0f1a4bb
Mathias Frohlich <mathias.frohlich@gmail.com>
2016-01-10 14:06:52 +0100

    Full Initial Commit
    Includes: code and README

---
+++ README.md
@@ -0,0 +1,27 @@
+# LaTeX Compile
+
+Compiles .tex to -pdf using pdflatex
+
+![Image of Command]()
+
+## How to install
+* Install [Visual Studio Code](https://code.visualstudio.com/)
+* Open `View -> Command Palette... and type ext install listFiles`
+* Restart Visual Studio Code and select the command "`LaTeX: Compile PDF`"
+  From the list of files displayed, select a file to open.
+
+## Working on:
+Tested on mac with [MacTex](https://tug.org/mactex/) installed.
+
+## Source
+
+[Github](https://github.com/mathiasfrohlich/vscode-LaTeXCompile)
+        
+## Change Log
+
+### Version 0.1.0
+Initial commit
+        
+## License
+
+[MIT](https://raw.githubusercontent.com/mathiasfrohlich/vscode-LaTeXCompile/master/LICENSE)
---
+++ extension.js
@@ -0,0 +1,82 @@
+/ The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+var vscode = require('vscode');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+	
+	var pdflatex = vscode.commands.registerCommand('extension.latexCompile', function () {	
+		
+		try {
+			var pathFull = vscode.window.activeTextEditor.document.fileName;
+			var path = getFilePath(pathFull); // get the current file path
+			var fileNameAndType = getFileNameAndType(pathFull);
+			// var fileName = getFileName(pathFull);
+			// var fileType = getFileType(pathFull);
+
+			if(getFileType(pathFull) != "tex"){
+				throw new Error("Can't create PDF, open a .tex file.")
+
+			}
+
+			console.log('cd ' + path + ' && pdflatex ' + fileNameAndType);
+			
+			var exec = require('child_process').exec,
+			cmd = exec('cd ' + path + ' && pdflatex ' + fileNameAndType);
+			
+
+			
+
+			cmd.stdout.on('data',function (data) {
+				console.log(String(data));
+				if (String(data).toLowerCase().indexOf("error") > 0){
+					vscode.window.showErrorMessage("Can't create PDF, see " + getFileName(pathFull) + ".log");
+				}
+				}
+			);
+			
+				vscode.window.showInformationMessage('PDF Compilled');
+			
+		} catch (error) {
+			vscode.window.showErrorMessage(error.message);
+			// vscode.window.showErrorMessage("Can't create PDF, open a .tex file.");
+		}
+	});
+	function errorHandeling(data){
+		console.log(String(data));
+	}
+	function getFileNameAndType(file) {
+        var forwardSlash = file.lastIndexOf("/");
+        var backSlash = file.lastIndexOf("\\");
+        if (forwardSlash === -1 && backSlash === -1) {
+            return file;
+        }
+        return file.substring((forwardSlash > backSlash) ? forwardSlash + 1 : backSlash + 1);
+    }
+	function getFileType(file) {
+        var stringFile = getFileNameAndType(file);
+		return stringFile.split(".")[1];
+    }
+	function getFileName(file) {
+        var stringFile = getFileNameAndType(file);
+		return stringFile.split(".")[0];
+    }
+	function getFilePath(file) {
+        var path = file;
+		path = path.match(/(.*)[\/\\]/)[1]||''; // extract the directory from the path
+		path += '/'
+		return path;
+    }
+	function setFlag(bool){
+		console.log(String("setflag "+bool));
+		errorFlag = bool;
+		console.log(String("setflagEr "+errorFlag));
+	}
+	context.subscriptions.push(pdflatex);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
